9ebb6fb75c77b2c2154a0c668dedc706612179c6
Obaid <ephiram2002@gmail.com>
2022-05-28 00:58:29 +0530

    Added Seperate Folder for Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
+import TopicsListPage from './pages/TopicsListPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TopicsListPage />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ frontend/src/assets/data.js
@@ -0,0 +1,16 @@
+let problems = [
+    {
+        'name': 'Level Order Traversal',
+        'type': 'Trees'
+    },
+    {
+        'name': 'Invert a Binary Tree',
+        'type': 'Trees'
+    },
+    {
+        'name': 'Find Max Height of a Tree',
+        'type': 'Trees'
+    }
+]
+
+export default problems;
