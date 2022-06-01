eb23e74e6f7140e7192a01cbb7205860a2aa392a
unknown <faru2103@gmail.com>
2022-06-01 11:03:19 +0530

    Topic Cards added

--- frontend/src/pages/TopicsListPage.js
+++ frontend/src/pages/TopicsListPage.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Grid from '@mui/material/Grid'
 import Topic from '../components/Topic';
 import topics from '../assets/topics';
-
+import topicsproblem from '../assets/data'
+import DummyTopic from '../components/DummyTopic';
 const TopicsListPage = () => {
   return (
-    <div>
+    <div  style={{ background: "linear-gradient(#e66465, #9198e5)" }}>
       <Grid container spacing={4} paddingTop={5} paddingLeft={5} paddingRight={5}>
-        {topics.map((topic, index) => (
+        {/* {topics.map((topic, index) => (
           <Grid item xs={12} md={6} lg={4}>
-            <Topic key={index} topic={topic}/>
+             /* <Topic key={index} topic={topic}/>  
+         
+          </Grid>
+        ))} */}
+         {topicsproblem.map((topic, index) => (
+          <Grid item xs={12} md={6} lg={4}>
+           
+             <DummyTopic key={index} topic={topic}/> 
           </Grid>
         ))}
       </Grid>
