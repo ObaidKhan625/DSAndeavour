3819ce99fdfd7431f9066ab3f55dc76096a7ae26
unknown <faru2103@gmail.com>
2022-06-03 18:30:12 +0530

    Error Merge

---
+++ frontend/src/assets/data.js
@@ -0,0 +1,163 @@
+let topicsproblem = [
+    {
+        //HELLO WORLD
+        'index':'1',
+        'name': 'ARRAYS 1',
+        'type': 'primary',
+        
+    },
+    {
+        'index':'2',
+        'name': 'ARRAYS 2',
+        'type': 'secondary'
+    },
+    {
+        'index':'3',
+        'name': 'ARRAYS 3',
+        'type': 'success'
+    },
+    {
+        'index':'4',
+        'name': 'ARRAYS 4',
+        'type': 'danger'
+    },
+    {
+        'index':'5',
+        'name': 'LINKED LIST',
+        'type': 'warning'
+    },
+    {
+        'index':'6',
+        'name': 'LINKED LIST 2',
+        'type': 'info'
+    },
+    {
+        'index':'7',
+        'name': 'LINKED LIST AND ARRAY',
+        'type': 'success'
+    },
+    {
+        'index':'8',
+        'name': 'GREDDY ALGORITHM',
+        'type': 'dark'
+    },
+    {
+        'index':'9',
+        'name': 'RECURSION',
+        'type': 'primary'
+    },
+    {
+        'index':'10',
+        'name': 'RECURSION AND BACKTRACK',
+        'type': 'secondary'
+    },
+    {
+        'index':'11',
+        'name': 'BINARY SEARCH',
+        'type': 'success'
+    },
+    {
+        'index':'12',
+        'name': 'HEAPS',
+        'type': 'danger'
+    },
+    {
+        'index':'13',
+        'name': 'STACKS AND QUEUE',
+        'type': 'warning'
+    },
+    {
+        'index':'14',
+        'name': 'STACKS AND QUEUE 2',
+        'type': 'info'
+    },
+    {
+        'index':'15',
+        'name': 'STRING',
+        'type': 'secondary'
+    },
+    {
+        'index':'16',
+        'name': 'STRING 2',
+        'type': 'dark'
+    },
+    {
+        'index':'17',
+        'name': 'BINARY TREE',
+        'type': 'primary'
+    },
+    {
+        'index':'18',
+        'name': 'BINARY TREE 2',
+        'type': 'dark'
+    },
+    {
+        'index':'19',
+        'name': 'BINARY TREE 3',
+        'type': 'success'
+    },
+    {
+        'index':'20',
+        'name': 'BINARY SEARCH TREE ',
+        'type': 'warning'
+    },
+    {
+        'index':'21',
+        'name': 'BINARY SEARCH TREE 2',
+        'type': 'danger'
+    },
+    {
+        'index':'22',
+        'name': 'BINARY SEARCH TREE (MISCELLAEOUS) ',
+        'type': 'info'
+    },
+    {
+        'index':'23',
+        'name': 'GRAPH',
+        'type': 'success'
+    },
+    {
+        'index':'24',
+        'name': 'GRAPH 2',
+        'type': 'dark'
+    },
+    {
+        'index':'25',
+        'name': 'DYNAMIC PROGRAMMING',
+        'type': 'primary'
+    },
+    {
+        'index':'26',
+        'name': 'DYNAMIC PROGRAMMING 2',
+        'type': 'secondary'
+    },
+    {
+        'index':'27',
+        'name': 'TRIE',
+        'type': 'success'
+    },
+    {
+        'index':'28',
+        'name': 'OPERATING SYSTEM REVISION',
+        'type': 'warning'
+    },
+    {
+        'index':'29',
+        'name': 'DBMS REVISION',
+        'type': 'info'
+    },
+    {
+        'index':'30',
+        'name': 'COMPUTER NETWORK REVISION',
+        'type': 'danger'
+    },
+    {
+        'index':'31',
+        'name': 'PROJECT OVERVIEW',
+        'type': 'dark'
+    },
+   
+   
+]
+
+export default topicsproblem;
