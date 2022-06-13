de71daaf0f0911fb5d62585f206eeb7299b2642a
unknown <faru2103@gmail.com>
2022-06-14 02:00:18 +0530

    Sweet alert

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,54 +1,44 @@
 import React, { useContext } from 'react'
 import AuthContext from '../context/AuthContext';
-import GoogleLogin from "react-google-login";
-import axios from "axios";
-const baseURL = "http://localhost:8000";
-
-// const handleGoogleLogin = (response) => {
-//   axios
-//     .post(`${baseURL}/auth/convert-token`, {
-//       token: response.accessToken,
-//       backend: "google-oauth2",
-//       grant_type: "convert_token",
-//       client_id: drfClientId,
-//       client_secret: drfClientSecret,
-//     })
-//     .then((res) => {
-//       const { access_token, refresh_token } = res.data;
-//       console.log({ access_token, refresh_token });
-//       localStorage.setItem("access_token", access_token);
-//       localStorage.setItem("refresh_token", refresh_token);
-//     })
-//     .catch((err) => {
-//       console.log("Error Google login", err);
-//     });
-// };
 
 const LoginPage = () => {
   let { loginUser } = useContext(AuthContext);
   return (
-    <div>
-      <form onSubmit={loginUser}>
-          <input type="text" name="username" placeholder="Enter Username"/>
-          <input type="password" name="password" placeholder="Enter Password"/>
-          <input type="submit"/>
-      </form>
-      {/* <GoogleLogin
-        clientId={googleClientId}
-        buttonText="LOGIN WITH GOOGLE"
-        onSuccess={(response) => handleGoogleLogin(response)}
-        render={(renderProps) => (
-          <button
-            onClick={renderProps.onClick}
-            disabled={renderProps.disabled}
-            type="button"
-            class="login-with-google-btn"
-          >
-            Sign in with Google
-          </button>
-        )}
-        onFailure={(err) => console.log("Google Login failed", err)}
-      /> */}
+    <div  style={{
+      paddingTop: '200px',
+      boxSizing: 'content-box',
+    }}>
+          <div className="outer">
+          <div className="inner">
+  
+  <form onSubmit={loginUser}>
+
+<h3>LOG IN</h3>
+&nbsp;
+&nbsp;
+<div className="form-group">
+    <label>Username</label>
+    <input type="text" name='username' className="form-control" placeholder="Enter email" />
+</div>
+&nbsp;
+&nbsp;
+<div className="form-group">
+    <label>Password</label>
+    <input type="password" name='password' className="form-control" placeholder="Enter password" />
+</div>
+&nbsp;
+&nbsp;
+
+ <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button> 
+ <p className="forgot-password text-right">
+  <b>SIGN IN WITH GOOGLE</b>
+</p>
+</form> 
+
+</div>
+</div>
+
+      
     </div>
   )
 }
