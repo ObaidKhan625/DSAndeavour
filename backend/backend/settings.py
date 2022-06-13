d45940f8413967a9a0d2dccde6e3107e6cd9c07e
Obaid <ephiram2002@gmail.com>
2022-06-14 01:37:51 +0530

    Added JWT token auth in Backend and cleaned up assets

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds = 5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes = 5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=100),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
